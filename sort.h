1fecee5c428d5c189d181c8a4d190008f7fc0158
OnyeEbube <okontaonyeka@gmail.com>
2022-11-09 11:53:46 +0000

    Update

--- sort.h
+++ sort.h
@@ -50,7 +50,8 @@ void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
 void cocktail_sort_list(listint_t **list);
 int get_max(int *array, int size);
 void counting_sort(int *array, size_t size);
-void merge_subarr(int *subarr, int *buff, size_t front, size_t mid, size_t back);
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back);
 void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
 void merge_sort(int *array, size_t size);
 void max_heapify(int *array, size_t size, size_t base, size_t root);
@@ -58,7 +59,8 @@ void heap_sort(int *array, size_t size);
 int get_max(int *array, int size);
 void radix_counting_sort(int *array, size_t size, int sig, int *buff);
 void radix_sort(int *array, size_t size);
-void bitonic_merge(int *array, size_t size, size_t start, size_t seq, char flow);
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
+		char flow);
 void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
 void bitonic_sort(int *array, size_t size);
 
