49a88339fe67b5d9c940420d78cdfcf8a36080b6
OnyeEbube <okontaonyeka@gmail.com>
2022-11-09 07:23:52 +0000

    Adding 100-shell_sort.c

---
+++ 100-shell_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * swap - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap(int *x, int *y)
+{
+	int tmp;
+
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+/**
+ * shell_sort - sorts an array of integers in ascending order
+ * using the Shell sort algorithm, using the Knuth sequence
+ * @array: the array to be sorted
+ * @size: the size of the array
+ *
+ * Return: no return
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
+	for (; gap >= 1; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
+		print_array(array, size);
+	}
+}
--- sort.h
+++ sort.h
@@ -29,4 +29,5 @@ void selection_sort(int *array, size_t size);
 int lomuto_partition(int *array, size_t size, int low, int high);
 void lomuto_sort(int *array, size_t size, int low, int high);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 #endif /* SORT_H */
